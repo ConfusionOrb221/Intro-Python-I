e442a784c808847fd6a0720a5b1307931d6a1034
Nathan <nate1898@gmail.com>
2020-09-01 23:44:26 -0400

    Module 1 first day
    What i was able to complete on the first day

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-
+f = open('foo.txt', 'r')
+for line in f:
+    print(line, end='')
+f.close()
+print()
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.txt', 'w')
+f.write('This is some text\n')
+f.write('This is a grand adventure\n')
+f.write('Into python\n')
+f.close()
+f = open('bar.txt', 'r')
+for line in f:
+    print(line, end='')
